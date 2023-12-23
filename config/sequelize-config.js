7f15cd2b582233453889c2ed265c6a5207a34e89
vuksijakovic <115589578+vuksijakovic@users.noreply.github.com>
2023-12-23 17:54:39 +0100

    Update sequelize-config.js

--- config/sequelize-config.js
+++ config/sequelize-config.js
@@ -1,8 +1,8 @@
 
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('kafic_baza', 'root', '', {
-  host: 'localhost',
+const sequelize = new Sequelize('bkgzuhxnutdwl9sjnwgj', 'ugo324al7kpbdd1g', 'Mm5a8PMmud9nUgX9a4EK', {
+  host: 'bkgzuhxnutdwl9sjnwgj-mysql.services.clever-cloud.com',
   dialect: 'mysql',
 
 });
